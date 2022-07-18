667ae464575b260c20937b1dba4d64c706df5ca3
Adelin <adelin.marin04@yahoo.com>
2022-07-18 08:35:29 +0300

    updated portfolio

--- src/components/Skills_Template.js
+++ src/components/Skills_Template.js
@@ -7,16 +7,14 @@ const Skills_Template = ({ Languages, Frameworks, Tools, DataBases }) => {
   return (
     <div className="skills-container">
       {/* {console.log("skill", skills)} */}
-      
+
       <p className="languages">Languages</p>
       <div className="wrapper-skills">
         {Languages.map((language, index) => {
           return (
             <div className="skill" key={index}>
               <img className="skill-photo" src={language.img} alt="css" />
-              {/* <p className="skill-name">
-                {language.name}
-              </p> */}
+              <p className="skill-name">{language.name}</p>
             </div>
           );
         })}
@@ -28,9 +26,7 @@ const Skills_Template = ({ Languages, Frameworks, Tools, DataBases }) => {
           return (
             <div className="skill" key={index}>
               <img className="skill-photo" src={framework.img} alt="css" />
-              <p className="skill-name">
-                {framework.name}
-              </p>
+              <p className="skill-name">{framework.name}</p>
             </div>
           );
         })}
@@ -42,9 +38,7 @@ const Skills_Template = ({ Languages, Frameworks, Tools, DataBases }) => {
           return (
             <div className="skill" key={index}>
               <img className="skill-photo" src={tools.img} alt="css" />
-              <p className="skill-name">
-                {tools.name}
-              </p>
+              <p className="skill-name">{tools.name}</p>
             </div>
           );
         })}
@@ -56,9 +50,7 @@ const Skills_Template = ({ Languages, Frameworks, Tools, DataBases }) => {
           return (
             <div className="skill" key={index}>
               <img className="skill-photo" src={dataBases.img} alt="css" />
-              <p className="skill-name">
-                {dataBases.name}
-              </p>
+              <p className="skill-name">{dataBases.name}</p>
             </div>
           );
         })}
